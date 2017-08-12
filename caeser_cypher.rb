82a1f3eb06bef01a6355e34be912d456ffd52a6e
Bryn Bayliss <brynbayliss@hotmail.com>
2017-08-12 16:14:41 +0100

    completed caeser cypher function

---
+++ caeser_cypher.rb
@@ -0,0 +1,37 @@
+def shift_string(c, shift) 
+  #helper method to move character along using ascii table
+  c = c.ord  
+  if c + shift <= 122
+       c += shift
+       c.chr
+     else
+       c = c + shift - 122
+       c = 96 + c
+       c.chr
+     end
+end
+
+def caeser(string, shift)
+  #a method that takes a string and a shift factor and returns an encoded string
+  #by shifting each character along by the factor
+
+  caps = "A".."Z"
+  newstring= ""
+  chars = "a".."z"
+  string.each_char do |c|
+    if caps.include?(c)
+      c = c.downcase
+      c = shift_string(c, shift).upcase
+      newstring += c  
+    elsif  chars.include?(c)
+      c = shift_string(c, shift)
+      newstring += c
+    else
+      newstring += c
+    end      
+  end
+  newstring
+end
+
+puts caeser("What A string!", 5)
+
