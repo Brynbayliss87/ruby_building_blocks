89c5a4d8e4e32ffbc648a2b8ebd22895e2c2f6a2
Bryn Bayliss <brynbayliss@hotmail.com>
2017-08-18 22:56:14 +0100

    got stock picker to pass specs and fixed bug if lowest number is at end of array

--- spec/stock_picker_spec.rb
+++ spec/stock_picker_spec.rb
@@ -3,8 +3,8 @@ require "./stock_picker"
 
 describe "stock_picker" do
 
-  output = stock_picker([1, 2, 3, 4, 5, 6, 7])
+  output = stock_picker([3 ,5 ,2 ,2, 25, 1])
   it "picks the highest profit stocks" do
-    expect(output).to eq([1, 7])
+    expect(output).to eq([2, 4])
   end
 end
--- stock_picker.rb
+++ stock_picker.rb
@@ -2,11 +2,10 @@ def stock_picker(prices)
   #takes an array of stock prices
   #returns a pair of days representing the best day to buy and sell
 
-
-
   min = prices[0]
   max = prices[0]  
   result = []  
+  high_result = [0, 0]
 
   prices.each do |i|
     if i < min
@@ -17,11 +16,18 @@ def stock_picker(prices)
     end
    result[0] = min
    result[1] = max
+
+   if high_result[1] - high_result[0] < result[1] - result[0]
+     high_result[0] = result[0] 
+     high_result[1] = result[1] 
+   end
   end
-  result
+  high_result[0] = prices.find_index(high_result[0])
+  high_result[1] = prices.find_index(high_result[1])  
+  high_result
 end
     
-puts stock_picker([1,2,3,4,5]) 
+ 
     
     
   
