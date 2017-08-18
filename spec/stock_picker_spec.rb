07e4c5c04f555b90e1e02ef550dfbe431b67c84b
Bryn Bayliss <brynbayliss@hotmail.com>
2017-08-18 22:22:55 +0100

    wrote some of the code for stock picker

--- spec/stock_picker_spec.rb
+++ spec/stock_picker_spec.rb
@@ -2,6 +2,9 @@ require "spec_helper"
 require "./stock_picker"
 
 describe "stock_picker" do
-  it "picks the highest profit stocks"
-   
+
+  output = stock_picker([1, 2, 3, 4, 5, 6, 7])
+  it "picks the highest profit stocks" do
+    expect(output).to eq([1, 7])
+  end
 end
