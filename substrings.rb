5c7a95de0c463290e2dfa31c1c82286ad49ab1f7
Bryn Bayliss <brynbayliss@hotmail.com>
2017-08-24 21:23:44 +0100

    wrote substrings method to pass spec

---
+++ substrings.rb
@@ -0,0 +1,30 @@
+def substrings(string, dictionary)
+  # takes a string and a dictionary of words
+  # returns a hash with the number that each substring
+  # appears in the main string
+
+  special_char = "~`@#%^&*()_-+={}[]|\:;\"'<,>.?/$"
+
+  string.each_char do |char|
+    string.delete!(char) if special_char.include?(char)
+  end
+
+  split_words = string.split
+
+  dict_counter = {}
+
+  split_words.each do |word|
+    word = word.downcase
+
+    dictionary.each do |sub|
+
+      if dict_counter.include?(sub)
+        dict_counter[sub] += 1 if word.include?(sub)
+      else dict_counter[sub] = 1 if word.include?(sub)
+      end
+    end
+  end
+  dict_counter
+end
+
+
