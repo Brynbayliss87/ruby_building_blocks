b145d8e367bf4d0237274d6e08f2194bbffaa413
Bryn Bayliss <brynbayliss@hotmail.com>
2017-08-24 20:49:33 +0100

    added spec for substrings method'

--- spec/stock_picker_spec.rb
+++ spec/stock_picker_spec.rb
@@ -1,10 +1,10 @@
-require "spec_helper"
-require "./stock_picker"
+require 'spec_helper'
+require './stock_picker'
 
-describe "stock_picker" do
+describe 'stock_picker' do
 
-  output = stock_picker([3 ,5 ,2 ,2, 25, 1])
-  it "picks the highest profit stocks" do
+  output = stock_picker([3, 5, 2, 2, 25, 1])
+  it 'picks the highest profit stocks' do
     expect(output).to eq([2, 4])
   end
 end
