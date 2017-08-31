4dc818fae9a297275d4e7145bc3fee9d679f1b79
Bryn Bayliss <brynbayliss@hotmail.com>
2017-08-31 20:52:03 +0100

    refactored using rubocop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -5,9 +5,9 @@ def bubble_sort(arr)
 
   loop do
     swapped = false
-    (length-1).times do |i|
-      if arr[i] > arr[i+1]
-        arr[i], arr[i+1] = arr[i+1], arr[i]
+    (length - 1).times do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
         swapped = true
       end
     end
@@ -17,24 +17,20 @@ def bubble_sort(arr)
 end
 
 def bubble_sort_by(arr)
- # uses bubble sort to sort an array but takes a block
- # that subtracts two elements length to sort by length of string
- 
+  # uses bubble sort to sort an array but takes a block
+  # that subtracts two elements length to sort by length of string
+
   length = arr.length
   loop do
     swapped = false
-    (length-1).times do |i|
-      result = yield(arr[i], arr[i+1]) # yields to the block 
-      if result > 0  # sorts elements based on result from block subtraction    
-        arr[i], arr[i+1] = arr[i+1], arr[i] 
+    (length - 1).times do |i|
+      result = yield(arr[i], arr[i + 1]) # yields to the block
+      if result > 0 # sorts elements based on result from block subtraction
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
         swapped = true
       end
     end
-  break unless swapped
+    break unless swapped
   end
   arr
-end    
-  
-
-  
-
+end
