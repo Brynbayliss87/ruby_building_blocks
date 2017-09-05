b300bc3868cd58b6d97497173d4790a92b5b4bb8
Bryn Bayliss <brynbayliss@hotmail.com>
2017-09-05 11:51:15 +0100

    added before each block to spec

--- spec/enumerable_methods_spec.rb
+++ spec/enumerable_methods_spec.rb
@@ -1,13 +1,18 @@
 require 'spec_helper'
 require './enumerable_methods'
 
-result = []
-[1, 2, 3, 4, 5].my_each do |i|
-  result << i + 1
-end
 
-describe 'my_each' do
-  it 'iterates an array without using #each' do
-    expect(result).to eq([2, 3, 4, 5, 6])
+describe 'Enumerable' do
+  before(:each) do
+    @result = []
+  end
+
+  it 'my_each' do
+    [1, 2, 3, 4, 5].my_each do |i|
+      @result << i + 1
+    end
+    expect(@result).to eq([2, 3, 4, 5, 6])
   end
 end
+
+
